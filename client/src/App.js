0799a4cb4de0243511682ad955b77109f334bcba
Jas <c.jasminher@gmail.com>
2021-05-03 20:48:10 -0400

    Created simple functions to store the values of the inputs in their states

--- client/src/App.js
+++ client/src/App.js
@@ -16,19 +16,19 @@ function App() {
       <div className="information">
 
       <label>Name:</label>
-      <input type="text"></input>
+      <input type="text" onChange={(e)=>{setName(e.target.value)}}></input>
 
       <label>Age:</label>
-      <input type="number"></input>
+      <input type="number" onChange={(e)=>{setAge(e.target.value)}}></input>
 
       <label>Country:</label>
-      <input type="text"></input>
+      <input type="text" onChange={(e)=>{setCountry(e.target.value)}}></input>
 
       <label>Position:</label>
-      <input type="text"></input>
+      <input type="text" onChange={(e)=>{setPosition(e.target.value)}}></input>
 
       <label>Wage (year):</label>
-      <input type="number"></input>
+      <input type="number" onChange={(e)=>{setWage(e.target.value)}}></input>
 
       <button>Add Employee</button>
       </div>
