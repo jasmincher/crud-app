fe9749da6fe4d18dffe98ff279c254d58c1f6297
Jas <c.jasminher@gmail.com>
2021-05-03 20:35:32 -0400

    Added labels and inputs layout for adding an employee info

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+
+      <div className="information">
+
+      <label>Name:</label>
+      <input type="text"></input>
+
+      <label>Age:</label>
+      <input type="number"></input>
+
+      <label>Country:</label>
+      <input type="text"></input>
+
+      <label>Position:</label>
+      <input type="text"></input>
+
+      <label>Wage (year):</label>
+      <input type="number"></input>
+
+      <button>Add Employee</button>
+      </div>
+
+
     </div>
   );
 }
